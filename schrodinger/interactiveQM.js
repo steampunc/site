933c1bc2f985dceca95211434f2066eadd4fccd1
Finn Boire <finn.boire@gmail.com>
2019-12-13 12:27:20 -0800

    Make it so darn close but still so far

--- schrodinger/interactiveQM.js
+++ schrodinger/interactiveQM.js
@@ -5,12 +5,13 @@ var canvasOffset=$("#display").offset();
 var offsetX=canvasOffset.left;
 var offsetY=canvasOffset.top;
 var tolerance=50;
+var errorTol = 0.001;
 var dragging=false;
 var rightClick=false;
 var px = 0;
 var py = 0;
 var EXP = 2.71828;
-var density = canvas.width / 3;
+var density = canvas.width / 10;
 var stretch = canvas.width / density;
 
 function sorter(p1, p2) {
@@ -19,8 +20,8 @@ function sorter(p1, p2) {
 
 class Point {
     constructor(x, y, draggable) {
-	this.x = x;
-	this.y = y;
+	this.x = Math.floor(x);
+	this.y = Math.floor(y);
 	this.draggable = draggable;
     }
 }
@@ -109,8 +110,10 @@ class Line {
 	}
     }
 
-    asPotential(x) {
-	return 0;
+    asWavefunction(x) {
+	var array = [];
+	var p1 = this.pointList[0];
+	var p2 = this.pointList[1];
     }
 
 }
@@ -133,43 +136,181 @@ class Complex {
 	return new Complex(this.r + cNum.r, this.im + cNum.im);
     }
 
+    addInv(cNum) {
+	return new Complex(this.r - cNum.im, this.im + cNum.r);
+	
+    }
+
+    sub(cNum) {
+	return new Complex(this.r - cNum.r, this.im - cNum.im);
+    }
+
     multiply(num) {
 	return new Complex(this.r * num, this.im * num);
     }
+
+    dot(cNum) {
+	return this.r * cNum.r + this.im * cNum.im;
+    }
+
+    copy() {
+	return new Complex(this.r, this.im);
+    }
 }
+
+function plotArray(array, height, scale) {
+    for (var i = 0; i < density + 1; i++) {
+	var p1 = new Point(stretch * i, canvas.height * height - scale * array[i]);
+	var p2 = new Point(stretch * (i + 1), canvas.height * height - scale * array[i + 1]);
+	lines.drawSegment(p1, p2);
+    }
+
+}
+
 
 class Wavefunction {
     constructor() {
 	this.wf = [];
-	this.pastSteps = [];
-	for (var x = 0; x < density; x++) {
+	var sum = 0;
+	for (var x = 0; x < density + 1; x++) {
 	    var pos = (x / density - 1/2);
 	    var width = 0.01;
 	    //this.wf.push(new Complex(Math.sin(x * 3.1415926 / density), 0));
-	    this.wf.push(new Complex(Math.pow(EXP, - pos * pos / width), 0));
-	    //var coord = ((x * 10 / density) - 0.9) * 2;
-	    //this.wf.push(new Complex(Math.pow(EXP, - coord * coord), Math.pow(EXP, -coord * coord)));
+	    //this.wf.push(new Complex(Math.pow(EXP, - pos * pos / width), 0));
+	    var coord = ((x * 10 / density) - 0.9) * 2;
+	    this.wf.push(new Complex(Math.pow(EXP, - coord * coord), Math.pow(EXP, -coord * coord)));
 	    this.time = 0;
-	    this.pastSteps.push(new Complex(0,0));
+	    this.dt = 1.0;
+	    sum += this.wf[x].mag();
+	}
+
+	for (var i = 0; i < density + 1; i++) {
+	    this.wf[i] = this.wf[i].multiply(1/sum);    
 	}
     }
 
-    evolve() {
-	var newWf = [new Complex(0, 0)];
-	for (var i = 1; i < density - 1; i++) {
-	    var step = 0; 
-	    step = this.wf[i - 1].add(this.wf[i + 1]).add(this.wf[i].multiply(-2));
-	    if (step.mag() > 0.01) {
-		step.multiply(0.01);
-	    } else if (step.mag() < this.wf[i].mag() * 0.0001) {
-		step.multiply(0);
-	    }
-	    newWf.push(this.wf[i].add(new Complex(-step.im, step.r)));	    
-	    this.pastSteps[i] = step;
+    secDer(wf, i, diff, scale) {
+	var nums = [];
+	var lower = -2;
+	var upper = 2;
+	if (i == 0) {
+	    lower = 0;
+	    var d = wf[0].sub(wf[1]);
+	    var left = wf[0].add(d);
+	    nums.push(left.add(d));
+	    nums.push(left);
 	}
-	newWf.push(new Complex(0, 0));
-	this.wf = newWf;
-	this.time += 0.01;
+	if (i == 1) {
+	    var left = wf[0].add(wf[0].sub(wf[1]));
+	    nums.push(left);
+	    lower = -1;
+	}
+	if (i == density - 1) {
+	    upper = 1;
+	    
+	}
+	if (i == density) {
+	    upper = 0;
+	}
+	for (var j = lower; j <= upper; j++) {
+	    var d = new Complex(0, 0);
+	    if (diff != 0) {
+		d = diff[i + j];
+	    }
+	    nums.push(wf[i + j].addInv(d));
+	}
+	if (upper == 0) {
+	    var d = wf[i].sub(wf[i-1]);
+	    var right = wf[i].add(d);
+	    nums.push(right);
+	    nums.push(right.add(d));
+	} else if (upper == 1) {
+	    var d = wf[i + 1].sub(wf[i]);
+	    var right = wf[i + 1].add(d);
+	    nums.push(right);
+	}
+
+	return this.calcSecDir(nums).multiply(scale * 1/density);
+    }
+
+    calcSecDir(nums) {
+	var ll = nums[0].multiply(-1);
+	var l = nums[1].multiply(16);
+	var m = nums[2].multiply(-30);
+	var r = nums[3].multiply(16);
+	var rr = nums[4].multiply(-1);
+	var ld = nums[0].add(nums[2]).add(nums[1].multiply(-2));
+	var rd = nums[4].add(nums[2]).add(nums[3].multiply(-2));
+	var md = nums[1].add(nums[3]).add(nums[2].multiply(-2));
+	var fd = (ll.add(l).add(m).add(r).add(rr)).multiply(1/12);
+	return md;
+    }
+
+    smoothify(array) {
+	var window = [array[0], array[1], array[2]];
+	var smoothed = [array[0]];
+	for (var i = 1; i < array.length - 2; i++) {
+	    smoothed.push(this.avg(window));
+	    window.shift();
+	    window.push(array[i + 2]);
+	}
+	smoothed.push(this.avg(window));
+	smoothed.push(array[array.length - 1]);
+	console.log(smoothed);
+	return smoothed;
+    }
+
+    avg(arr) {
+	var a = new Complex(0, 0);
+	for (var i = 0; i < arr.length; i++) {
+	    a = a.add(arr[i]);
+	}
+	return a.multiply(1/arr.length);
+    }
+
+    evolve() {
+	// Using a super simple runge-kutta numerical approximation of second order
+	var k1 = [];
+	var dbg = [];
+	var k2 = [];
+	var tempWf = [];
+	for (var i = 0; i < density + 1; i++) {
+	    k1.push(this.secDer(this.wf, i, 0, 1).multiply(this.dt));
+	}
+	k1 = this.smoothify(k1);
+	for (var i = 0; i < density + 1; i++) {
+	    k2.push(this.secDer(this.wf, i, k1, 1).multiply(this.dt));
+	}
+	k2 = this.smoothify(k2);
+	var sum = 0;
+	// Calculate steps;
+	for (var i = 0; i < density + 1; i++) {
+	    var heun = (k1[i].add(k2[i])).multiply(0.5);
+	    dbg.push(heun.r);
+	    var euler = k1[i]
+	    var heunDiff = this.wf[i].addInv(heun).multiply(0.999);
+	    var eulerDiff = this.wf[i].addInv(euler);
+	    var error = heunDiff.sub(eulerDiff).mag();
+	    if (error > errorTol) {
+		console.log("Adjusting error");
+		this.dt = 0.9 * this.dt * Math.min(
+		    Math.max(Math.sqrt(errorTol / (2 * error)), 0.3), 1.3)
+		this.evolve();
+		return;
+	    } else {
+		tempWf.push(heunDiff);
+		sum += heunDiff.mag();
+	    }
+	}
+	this.dt = 0.9 * this.dt * Math.min(
+	    Math.max(Math.sqrt(errorTol / (2 * error)), 0.3), 1.3)
+	this.wf = tempWf;
+	// Renormalize
+	for (var i = 0; i < density + 1; i++) {
+	    this.wf[i] = this.wf[i].multiply(1/Math.sqrt(sum));    
+	}
+	console.log(sum);
+	this.time += this.dt;
     }
 
     display(prob) {
@@ -179,15 +320,17 @@ class Wavefunction {
 	}
 	var scale = canvas.height / (2.5 * Math.max.apply(Math, mags));
 
-	for (var i = 0; i < this.wf.length - 1; i++) {
-	    if (prob) {
-		var p1 = new Point(stretch * i, canvas.height / 2 - 70 * mags[i]);
-		var p2 = new Point(stretch * (i + 1), canvas.height / 2 - 70 * mags[i + 1]);
-		lines.drawSegment(p1, p2);
-	    } else {
-		var p1 = new Point(stretch * i, canvas.height / 2 - scale * this.wf[i].r);
-		var p2 = new Point(stretch * (i + 1), canvas.height / 2 - scale * this.wf[i + 1].r);
-		lines.drawSegment(p1, p2);
+	for (var i = 0; i < this.wf.length; i++) {
+	    if (i < this.wf.length - 1) {
+		if (prob) {
+		    var p1 = new Point(stretch * i, canvas.height / 2 - scale * mags[i]);
+		    var p2 = new Point(stretch * (i + 1), canvas.height / 2 - scale * mags[i + 1]);
+		    lines.drawSegment(p1, p2);
+		} else {
+		    var p1 = new Point(stretch * i, canvas.height / 2 - 10 * Math.sqrt(scale) * this.wf[i].r);
+		    var p2 = new Point(stretch * (i + 1), canvas.height / 2 - 10 * Math.sqrt(scale) * this.wf[i + 1].r);
+		    lines.drawSegment(p1, p2);
+		}
 	    }
 	}
     }
@@ -226,7 +369,7 @@ function handleMouseMove(e){
 var lines = new Line(canvas.width, canvas.height);
 var wavefunction = new Wavefunction();
 
-var showingProbs = true;
+var showingProbs = false;
 function redraw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     lines.draw();
@@ -249,11 +392,11 @@ $("#dispProbs").click(function() {
 
 var currRun = null;
 $("#fire").click(function() {
+    waveFunction = new Wavefunction();
+    console.log("RESET");
     clearInterval(currRun);
-    wavefunction = new Wavefunction();
-    wavefunction.time = 0;
-    currRun = setInterval(function () {
+    currRun = setInterval(function() {
 	wavefunction.evolve();
 	redraw();
-    }, 0.01);
+    }, 1);
 });
